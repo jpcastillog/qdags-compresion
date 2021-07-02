8e977458345f632dfeb22b840d97fa2ec919c6a0
yeip1 <juan.castillog@sansano.usm.cl>
2021-07-02 02:36:36 -0400

    Update inverted_index.cpp

--- queries/inverted_index.cpp
+++ queries/inverted_index.cpp
@@ -67,7 +67,7 @@ uint64_t maximum_in_table(std::vector<std::vector<uint64_t>> &table, uint16_t n_
 }
 
 int main(){
-    uint8_t coder = 0;
+    uint8_t coder = 1;
     const std::string input_file_name  = "ejemplo.txt";
     // const std::string input_file_name  = "./../../../../data/bitvectors/ii/gov2/url/gov2_ii_nofreq_url_dif.txt.B";
     
